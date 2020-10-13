0c4901851c6adb10a1f57b1f408775fc82d0416d
Andihamsah <ahamsah0@gmail.com>
2020-10-13 19:32:59 +0700

    view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,195 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="user-wrapper">
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">10</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://img.pngio.com/avatar-icon-png-105-images-in-collection-page-3-avatarpng-512_512.png" alt="" class="media-object" style="height: 80px">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">blabla</p>
+                                <p class="email">blabla@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-8" id="messages">
+            <div class="message-wrapper">
+                <ul class="messages">
+                    <li class="message clearfix">
+                        <div class="send">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="send">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="send">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="send">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero perspiciatis minima, iusto ad, delectus suscipit sint aut minus laudantium at, qui vero molestias adipisci exercitationem! Rem suscipit fuga vero non.</p>
+                            <p class="date">Selasa, 13 Oktober 2020</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <input type="text" name="message" class="submit">
+        </div>
+    </div>
+</div>
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
